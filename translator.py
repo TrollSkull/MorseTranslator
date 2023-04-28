326e656f4ae89b2651bfaf61ebf29e99ab810edb
TrollSkull <leanahuelmiranda@gmail.com>
2023-04-28 07:09:31 -0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,6 +4,22 @@ __author__ = 'TrollSkull'
 __version__= 'v2.0'
 
 try:
+    import requests
+except ImportError as error:
+    import time, os
+
+    print(error + '\nSome requirements are not installed.')
+    requirements = ['requests']
+
+    for requirement in requirements:
+
+        print(f'Installing "{requirement}"...')
+        os.system(f'pip3 install {requirement}')
+        time.sleep(1)
+
+    print('\nRequirements have been installed.')
+
+try:
     from lib.main import Main, main
     from lib.core.exceptions import MyExceptions
 except Exception as err:
